9cae2b652368327185af527c0bb935f8f2bb772c
Alban Peignier <alban@tryphon.eu>
2010-03-11 11:54:41 +0100

    Create a image with root in squashfs with a customize initramfs

---
+++ lib/system_builder/disk_squashfs_image.rb
@@ -0,0 +1,148 @@
+require 'tempfile'
+
+class SystemBuilder::LiveImage
+
+  attr_accessor :boot, :size
+  attr_reader :file
+
+  def initialize(file)
+    @file = file
+    @size = 512.megabytes
+  end
+
+  def create
+    boot.configurators << SystemBuilder::ProcConfigurator.new do |chroot|
+      puts "* install initramfs-tools"
+      chroot.apt_install %w{initramfs-tools syslinux}
+
+      script = "/usr/share/initramfs-tools/scripts/local-top/mount_boot"
+      unless chroot.image.exists?(script)
+        chroot.image.open(script) do |f|
+        f.puts <<EOF
+#!/bin/sh -x
+case ${1:-} in
+  prereqs) echo ""; exit 0;;
+esac
+mkdir /boot
+mount -t ext3 LABEL=#{fs_label} /boot
+exit 0
+EOF
+        end  
+
+        chroot.sudo "chmod +x #{script}"
+        chroot.sudo "/usr/sbin/update-initramfs -u"
+      end
+    end
+
+    boot.create
+
+    file_creation = (not File.exists?(file))
+    if file_creation
+      create_file
+      create_partition_table
+
+      format_boot_fs
+    end
+
+    sync_boot_fs
+    install_extlinux_files
+
+    compress_root_fs
+    install_extlinux
+
+    self
+  end
+
+  def create_file
+    FileUtils::sh "dd if=/dev/zero of=#{file} count=#{size.in_megabytes.to_i} bs=1M"
+  end
+
+  def create_partition_table
+    # Partition must be bootable for syslinux
+    FileUtils::sh "echo '#{free_sectors},,L,*' | /sbin/sfdisk --no-reread -uS -H16 -S63 #{file}"
+  end
+ 
+  def format_boot_fs
+    loop_device = "/dev/loop0"
+    begin
+      FileUtils::sudo "losetup -o #{boot_fs_offset} #{loop_device} #{file}"
+      FileUtils::sudo "mke2fs -L #{fs_label} -jqF #{loop_device} #{boot_fs_block_size}"
+    ensure
+      FileUtils::sudo "losetup -d #{loop_device}"
+    end
+  end
+
+  def mount_boot_fs(mount_dir = "/tmp/mount_boot_fs", &block)
+    FileUtils::mkdir_p mount_dir
+
+    begin
+      FileUtils::sudo "mount -o loop,offset=#{boot_fs_offset} #{file} #{mount_dir}"
+      yield mount_dir
+    ensure
+      FileUtils::sudo "umount #{mount_dir}"
+    end
+  end
+
+  def compress_root_fs
+    mount_boot_fs do |mount_dir|
+      FileUtils::sudo "mksquashfs #{boot.root}/ #{mount_dir}/filesystem.squashfs -noappend -e #{boot.root}/"
+    end
+    FileUtils.touch file
+  end
+  
+  def sync_boot_fs
+    mount_boot_fs do |mount_dir|
+      FileUtils::sudo "rsync -a --delete #{boot.root}/boot/ #{mount_dir}/"
+      FileUtils::sudo "ln -s #{readlink_boot_file('initrd.img')} #{mount_dir}/initrd.img"
+      FileUtils::sudo "ln -s #{readlink_boot_file('vmlinuz')} #{mount_dir}/vmlinuz.img"
+    end
+    FileUtils.touch file
+  end
+
+  def install_extlinux_files(options = {})
+    root = (options[:root] or "LABEL=#{fs_label}")
+    version = (options[:version] or Time.now.strftime("%Y%m%d%H%M"))
+
+    boot.image do |image|
+      image.mkdir "/boot/"
+
+      image.open("/boot/extlinux.conf") do |f|
+        f.puts "DEFAULT linux"
+        f.puts "LABEL linux"
+        f.puts "SAY Now booting #{version} from syslinux ..."
+        f.puts "KERNEL /vmlinuz"
+        f.puts "APPEND ro initrd=/initrd.img boot=local root=/boot/filesystem.squashfs rootflags=loop rootfstype=squashfs rootdelay=30 debug"
+      end
+    end
+  end
+
+  def install_extlinux
+    mount_boot_fs("#{boot.root}/boot") do 
+      boot.chroot do |chroot|
+        chroot.sudo "extlinux --install -H16 -S63 /boot"
+      end
+    end
+    FileUtils::sh "dd if=/usr/lib/extlinux/mbr.bin of=#{file} conv=notrunc"
+  end
+
+  def fs_label
+    "boot"
+  end
+
+  def readlink_boot_file(boot_file)
+    File.basename(%x{readlink #{boot.root}/#{boot_file}}.strip)
+  end
+
+  def free_sectors
+    64
+  end
+
+  def boot_fs_offset
+    free_sectors * 512
+  end
+
+  def boot_fs_block_size
+    linux_partition_info = `/sbin/sfdisk -l #{file}`.scan(%r{#{file}.*Linux}).first
+    linux_partition_info.split[5].to_i
+  end
+end
--- system-builder.gemspec
+++ system-builder.gemspec
@@ -6,13 +6,13 @@ Gem::Specification.new do |s|
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Alban Peignier"]
-  s.date = %q{2010-03-08}
+  s.date = %q{2010-03-11}
   s.default_executable = %q{system-builder}
   s.description = %q{FIX (describe your package)}
   s.email = ["alban@tryphon.eu"]
   s.executables = ["system-builder"]
   s.extra_rdoc_files = ["History.txt", "Manifest.txt", "PostInstall.txt"]
-  s.files = ["History.txt", "Manifest.txt", "PostInstall.txt", "README.rdoc", "Rakefile", "bin/system-builder", "examples/Rakefile", "examples/config.rb", "examples/manifests/classes/test.pp", "examples/manifests/site.pp", "features/development.feature", "features/step_definitions/common_steps.rb", "features/support/common.rb", "features/support/env.rb", "features/support/matchers.rb", "lib/system_builder.rb", "lib/system_builder/boot.rb", "lib/system_builder/cli.rb", "lib/system_builder/configurator.rb", "lib/system_builder/core_ext.rb", "lib/system_builder/disk_image.rb", "lib/system_builder/iso_image.rb", "lib/system_builder/live_image.rb", "lib/system_builder/task.rb", "script/console", "script/destroy", "script/generate", "spec/spec.opts", "spec/spec_helper.rb", "spec/system_builder/boot_spec.rb", "spec/system_builder/cli_spec.rb", "system-builder.gemspec", "tasks/rspec.rake"]
+  s.files = ["History.txt", "Manifest.txt", "PostInstall.txt", "README.rdoc", "Rakefile", "bin/system-builder", "examples/Rakefile", "examples/config.rb", "examples/manifests/classes/test.pp", "examples/manifests/site.pp", "features/development.feature", "features/step_definitions/common_steps.rb", "features/support/common.rb", "features/support/env.rb", "features/support/matchers.rb", "lib/system_builder.rb", "lib/system_builder/boot.rb", "lib/system_builder/cli.rb", "lib/system_builder/configurator.rb", "lib/system_builder/core_ext.rb", "lib/system_builder/disk_image.rb", "lib/system_builder/disk_squashfs_image.rb", "lib/system_builder/iso_image.rb", "lib/system_builder/live_image.rb", "lib/system_builder/task.rb", "script/console", "script/destroy", "script/generate", "spec/spec.opts", "spec/spec_helper.rb", "spec/system_builder/boot_spec.rb", "spec/system_builder/cli_spec.rb", "system-builder.gemspec", "tasks/rspec.rake"]
   s.homepage = %q{http://projects.tryphon.eu/system-builder}
   s.rdoc_options = ["--main", "README.rdoc"]
   s.require_paths = ["lib"]
@@ -25,17 +25,17 @@ Gem::Specification.new do |s|
     s.specification_version = 3
 
     if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
-      s.add_development_dependency(%q<rubyforge>, [">= 2.0.3"])
-      s.add_development_dependency(%q<gemcutter>, [">= 0.3.0"])
+      s.add_development_dependency(%q<rubyforge>, [">= 2.0.4"])
+      s.add_development_dependency(%q<gemcutter>, [">= 0.5.0"])
       s.add_development_dependency(%q<hoe>, [">= 2.5.0"])
     else
-      s.add_dependency(%q<rubyforge>, [">= 2.0.3"])
-      s.add_dependency(%q<gemcutter>, [">= 0.3.0"])
+      s.add_dependency(%q<rubyforge>, [">= 2.0.4"])
+      s.add_dependency(%q<gemcutter>, [">= 0.5.0"])
       s.add_dependency(%q<hoe>, [">= 2.5.0"])
     end
   else
-    s.add_dependency(%q<rubyforge>, [">= 2.0.3"])
-    s.add_dependency(%q<gemcutter>, [">= 0.3.0"])
+    s.add_dependency(%q<rubyforge>, [">= 2.0.4"])
+    s.add_dependency(%q<gemcutter>, [">= 0.5.0"])
     s.add_dependency(%q<hoe>, [">= 2.5.0"])
   end
 end
