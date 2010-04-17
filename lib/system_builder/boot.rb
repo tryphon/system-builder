83295d9cf22418710ef55f3b2ba8c337358f5b99
Alban Peignier <alban@tryphon.eu>
2010-04-17 17:33:05 +0200

    Modify /boot in fstab to detect format. Refs #7

--- lib/system_builder/boot.rb
+++ lib/system_builder/boot.rb
@@ -93,7 +93,7 @@ class SystemBuilder::DebianBoot
     SystemBuilder::ProcConfigurator.new do |chroot|
       puts "* create fstab"
       chroot.image.open("/etc/fstab") do |f|
-        f.puts "LABEL=boot /boot vfat defaults,noatime 0 1"
+        f.puts "LABEL=boot /boot auto defaults,noatime,ro 0 0"
         %w{/tmp /var/run /var/log /var/lock /var/tmp}.each do |directory|
           f.puts "tmpfs #{directory} tmpfs defaults,noatime 0 0"
         end
